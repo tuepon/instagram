38f0d9af72e34dc2f1da6fbacb3f885b8efa9c3b
tuepon <t.uepon@gmail.com>
2023-11-05 13:47:53 +0900

    update comment modal to update status after deleting realtime

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,17 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment = Comment.find(params[:id])
-    @comment.destroy = @comment.user && current_user
+    if (@comment.user == current_user)
+      @comment.destroy
+    end
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.remove(
+          "post#{@comment.post_id}ModalComment#{@comment.id}"
+        )
+      end
+    end
   end
 
   private
