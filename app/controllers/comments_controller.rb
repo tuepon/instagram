faade550ce468dc282daa4d156b87a4c65871d5e
tuepon <t.uepon@gmail.com>
2023-11-05 09:17:57 +0900

    update comments to show after post immediately

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,23 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only[:create]
+  before_action :set_post, only: [:create]
 
   def create
-    @post.comments.create(user: current_user, body: params[:comment_body])
+    @comment = @post.comments.create(user: current_user, body: params[:comment_body])
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "post#{@post.id}comments",
+          partial: 'posts/post_comments',
+          locals: { post: @post }
+        )
+      end
+    end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-    @comment.destroy if @comment.user == current_user
+    @comment.destroy = @comment.user && current_user
   end
 
   private
