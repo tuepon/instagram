1bf3bd754a1efd3ad9c186c7c20a26d0b78ba934
tuepon <t.uepon@gmail.com>
2023-11-11 17:50:25 +0900

    migrated follow model

--- db/migrate/20231111083033_create_follows.rb
+++ db/migrate/20231111083033_create_follows.rb
@@ -3,9 +3,13 @@ class CreateFollows < ActiveRecord::Migration[7.0]
     create_table :follows do |t|
       t.integer :follower_id
       t.integer :followed_id
-      t.boolean :accepted
+      t.boolean :accepted, default: false
 
       t.timestamps
     end
+
+    add_index :follows, :follower_id
+    add_index :follows, :followed_id
+    add_index :follows, [:follower_id, :followed_id], unique: true
   end
 end
