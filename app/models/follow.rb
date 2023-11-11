e692e2ef9d87f71347979a688c4c2e1fd8e75f85
tuepon <t.uepon@gmail.com>
2023-11-11 20:37:04 +0900

    add relationship between user and follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,4 @@
 class Follow < ApplicationRecord
+  belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
+  belongs_to :followed, class_name: 'User', foreign_key: 'followed_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,17 @@ class User < ApplicationRecord
   has_many :likes
 
   has_many :comments
+
+  has_many :follow_requests, -> { where(accepted: false) }, class_name: 'Follow', foreign_key: 'followed_id'
+
+  has_many :accepted_received_requests, -> { where(accepted: true) }, class_name: 'Follow', foreign_key: 'followed_id'
+
+  has_many :accepted_sent_requests, -> { where(accepted: true) }, class_name: 'Follow', foreign_key: 'follower_id'
+
+  # has_many :received_requests, class_name: 'Follow', foreign_key: 'followed_id'
+  # has_many :sent_requests, class_name: 'Follow', foreign_key: 'follower_id'
+  # has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: 'Follow', foreign_key: 'follower_id'
+
+  has_many :followers, through: :accepted_received_requests, source: :follower
+  has_many :followings, through: :accepted_sent_requests, source: :followed
 end
