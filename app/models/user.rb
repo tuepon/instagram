e3679b2c67c05adbcbda4643cba7a1ad57f8442a
tuepon <t.uepon@gmail.com>
2023-11-12 09:32:52 +0900

    update user model and follows controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
 
   # has_many :received_requests, class_name: 'Follow', foreign_key: 'followed_id'
   # has_many :sent_requests, class_name: 'Follow', foreign_key: 'follower_id'
-  # has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: 'Follow', foreign_key: 'follower_id'
+  has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: 'Follow', foreign_key: 'follower_id'
 
   has_many :followers, through: :accepted_received_requests, source: :follower
   has_many :followings, through: :accepted_sent_requests, source: :followed
@@ -31,5 +31,9 @@ class User < ApplicationRecord
   def unfollow(user)
     self.accepted_sent_requests.find_by(followed: user)&.destroy
   end
-  
+
+  def cancel_request(user)
+    self.waiting_sent_requests.find_by(followed: user)&.destroy
+  end
+
 end
