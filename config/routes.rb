9e1801e33f89c9726d1f0ad238b8d892718079db
tuepon <t.uepon@gmail.com>
2023-11-15 22:42:49 +0900

    create real time search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :comments, only: [:create, :destroy]
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 
   post "follow", to: "follows#follow", as: :follow
   delete "unfollow", to: "follows#unfollow", as: :unfollow
