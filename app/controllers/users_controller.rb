b3a6dbc18cf39622d64aed167c298fda1cc60436
tuepon <t.uepon@gmail.com>
2023-12-03 17:52:33 +0900

    update users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,10 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    unless @user
+      flash[:alert] = "User not found"
+      redirect_to root_path
+    end 
   end
 end
